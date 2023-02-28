eee5c05d917cf201f8a241aed19ea9d24b379e3e
Alex <57497005+KingDevCode@users.noreply.github.com>
2023-02-28 21:17:32 +0100

    Scoreboard update

--- Leaderboard.js
+++ Leaderboard.js
@@ -12,27 +12,40 @@ class Leaderboard {
     const players = await response.json();
 
     for (let player of players) {
-      const playerDiv = document.createElement("div");
-      playerDiv.classList.add("leaderboard-player");
-
-      const playerName = document.createElement("h2");
-      playerName.classList.add("leaderboard-player-name");
-      playerName.innerHTML = player.user;
-      const playerScore = document.createElement("p");
-      playerScore.classList.add("leaderboard-player-score");
-      playerScore.innerHTML = player.highscore;
-
-      playerDiv.appendChild(playerName);
-      playerDiv.appendChild(playerScore);
-
-      this.#leaderboard.appendChild(playerDiv);
+      this.#leaderboard.appendChild(this.#getPlayerElement(player));
     }
 
     document.body.appendChild(this.#leaderboard);
   }
-  update() {
-    this.#leaderboard.innerHTML = "";
-    this.#init();
+  #getPlayerElement(player) {
+    const playerDiv = document.createElement("div");
+    playerDiv.classList.add("leaderboard-player");
+
+    const playerName = document.createElement("h2");
+    playerName.classList.add("leaderboard-player-name");
+    playerName.innerHTML = player.user;
+    playerName.id = player.user;
+
+    const playerScore = document.createElement("p");
+    playerScore.classList.add("leaderboard-player-score");
+    playerScore.innerHTML = player.highscore;
+    playerScore.id = player.user + "-score";
+
+    this.#playerDiv.appendChild(playerName);
+    this.#playerDiv.appendChild(playerScore);
+
+    return playerDiv;
+  }
+  async update() {
+    const response = await fetch("https://colourfinder.onrender.com/");
+    const players = await response.json();
+
+    for (let player of players) {
+      const score = document.getElementById(player.user + "-score");
+      if (score === undefined)
+        this.#leaderboard.appendChild(this.#getPlayerElement(player));
+      else score.innerHTML = player.score;
+    }
   }
   show() {
     this.#leaderboard.style.display = "block";
