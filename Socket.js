23e648b475bb239b86c44fb83d8ac8287443e56a
Alex <57497005+KingDevCode@users.noreply.github.com>
2023-03-01 16:33:30 +0100

    Update Socket.js

--- Socket.js
+++ Socket.js
@@ -1,6 +1,6 @@
 class Socket {
-  // static socket = io("https://colourfinder.onrender.com");
-  static socket = io("localhost:3030");
+  static socket = io("https://colourfinder.onrender.com");
+  //static socket = io("localhost:3030");
   static get() {
     return Socket.socket;
   }
